7570b41613fcae1bcb0f9410426296ec9611829c
d1y <chenhonzhou@gmail.com>
2020-06-02 01:17:41 +0800

    添加了两个公共组件
    1. 卡片添加了信息
    2. 添加了自定义 `bar`

--- src/components/card.vue
+++ src/components/card.vue
@@ -6,13 +6,25 @@
         <text class="text-xl text-bold">{{ title }}</text>
       </view>
     </view>
-    <scroll-view scroll-x>
+    <scroll-view scroll-x class="bg-white nav" scroll-with-animation>
+      <view class="grid col-3 diy-grid">
+        <view class="cu-item grid-card-item" v-for="(item,index) in data" :key="index">
+          <view class="bg-img bg-mask" :style="{
+            backgroundImage: `url(${ item.cover })`,
+            height: `320upx`
+          }"></view>
+          <!-- <image :src="item.cover" /> -->
+          <view class="text-cut">{{ item.title }}</view>
+        </view>
+      </view>
+		</scroll-view>
+    <!-- <scroll-view scroll-x>
       <view class="grid margin-bottom text-center col-3">
         <view style="height: 10vh" v-for="(item, index) in 20" :key="index">
           {{ item }}
         </view>
       </view>
-    </scroll-view>
+    </scroll-view> -->
   </view>
 </template>
 
@@ -32,6 +44,12 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.diy-grid {
+  flex-wrap: nowrap
+}
+.grid-card-item {
+  margin: unset;
+  height: auto;
+}
 </style>
