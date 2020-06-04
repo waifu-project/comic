d10dca968bdfeb8d12e8e555058519a83e111a17
d1y <chenhonzhou@gmail.com>
2020-06-05 01:05:03 +0800

    添加公共接口

---
+++ src/api/share.ts
@@ -0,0 +1,28 @@
+import { get } from '@/utils/axios'
+
+// 参考: https://developer.hitokoto.cn
+interface sayWordInterface {
+  created_at?: string // 创建时间
+  creator?: string // 未知
+  creator_uid?: number // 创建id
+  from?: string // 来自
+  from_who?: string // 未知
+  hitokoto?: string // say
+  id?: number
+  reviewer?: number // 阅读量
+  type?: number // lei'xi
+  uuid?: string
+}
+
+uni.getSystemInfoSync
+
+// 一言
+export const getWord = async (): Promise<sayWordInterface>=> {
+  try {
+    const data: sayWordInterface = await get('https://v1.hitokoto.cn/')
+    return data
+  } catch (error) {
+    console.error(error)
+    return {}
+  }
+}
