ad28f0316951ba52490a6c5fe90d6d8a7de988ac
d1y <chenhonzhou@gmail.com>
2020-06-05 01:10:31 +0800

    配合镜像使用, 并重新封装了 get() 方法

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -6,20 +6,34 @@ const axios = require('@/plugins/axios')
 
 const { baseUrl } = config
 
-axios.setConfig({ baseUrl, debug })
+axios.setConfig({
+  baseUrl,
+  debug,
+  skipInterceptorResponse: false
+})
 
 axios.interceptor.request = (req: any)=> {
   uni.showLoading()
   return req
 }
 
+// TODO 取消响应拦截器
 axios.interceptor.response = (res: any) => {
   uni.hideLoading()
-  if (res.code === 0) res.success = true
+  // if (res.code === 0) res.success = true
+  // res.success = true
+  // res.result = res
   return res
 }
 
-export default ins
+export default axios
 
-export const get = (url: string)=> axios.get({ url })
-export const post = (data: postBodyFace)=> axios.post(data)
+export const get = (url: string): Promise<any>=> new Promise(res=> {
+  _get(url).then(null, (fastData: any)=> {
+    const { data } = fastData
+    res(data)
+  })
+})
+
+export const _get = (url: string)=> axios.get({ url })
+export const _post = (data: postBodyFace)=> axios.post(data)
