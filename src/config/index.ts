464e3244b197faefa0ee30e87f85783707b3d8ba
d1y <chenhonzhou@gmail.com>
2020-06-05 22:29:20 +0800

    添加设置页面, 添加 version 字段

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,13 @@ export default {
   baseUrl
 }
 
+export let version: string = ''
+export let readme: string = ''
+try {
+  const _P: any = require('../../package.json')
+  version = _P.version
+} catch (error) {
+  console.error('读取失败, 错误栈: ', error)
+}
+
 export const isDev = process.env.NODE_ENV === 'development'
--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -1,16 +1,60 @@
 <template>
   <view>
-    
+    <topbar>
+      <block slot="content"> {{ '设置' }} </block>
+    </topbar>
+    <wrapper>
+
+      <view class="cu-bar solid-bottom bar_box padding-bottom-xs">
+        <view class="action">
+          <text class="cuIcon-title text-orange" />
+          {{ '阅读设置' }}
+        </view>
+      </view>
+      <view class="cu-list menu">
+        <view class="cu-item">
+          <view class="content">
+            夜间模式
+          </view>
+          <view class="action">
+            <switch checked color="var(--pink)"/>
+          </view>
+        </view>
+      </view>
+
+      <view class="cu-bar solid-bottom bar_box padding-bottom-xs margin-top-sm">
+        <view class="action">
+          <text class="cuIcon-title text-orange" />
+          {{ '其他设置' }}
+        </view>
+      </view>
+      <view class="cu-list menu">
+        <view class="cu-item">
+          <view class="content">
+            版本号
+          </view>
+          <view class="action">
+            0.0.1
+          </view>
+        </view>
+      </view>
+
+    </wrapper>
   </view>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import * as config from '@/config'
 export default Vue.extend({
-  
+  created() {
+    console.log('config: ', config);
+  }
 })
 </script>
 
-<style>
-
+<style scoped>
+.bar_box {
+  min-height: 42rpx;
+}
 </style>
