5443cb2a4361c76adf85e715a62d571ff4e08c01
d1y <chenhonzhou@gmail.com>
2020-06-07 22:45:36 +0800

    添加 `app` 图标配置

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,16 +1,41 @@
 import { shareComicFace } from '@/interface';
 
 interface searchDataInterface {
+  /**
+   * `title-bar` 文字
+   */
+  barTitle: string
+  /**
+   * 搜索的接口
+   */
   url: string
+  /**
+   * 页数
+   */
   page: number
+  /**
+   * 全部页数
+   */
   total_page: number
+  /**
+   * 是否还有下一页
+   */
   isNext: boolean
 }
 
 // 漫画接口
 export interface comicInterface {
+  /**
+   * 搜索数据
+   */
   searchData: searchDataInterface
+  /**
+   * 历史观看
+   */
   history_aviews: shareComicFace[]
+  /**
+   * 收藏
+   */
   collect_lists: shareComicFace[]
 }
 
