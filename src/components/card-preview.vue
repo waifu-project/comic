8b0d54e8f11240336837bb70f6d534805538b07e
d1y <chenhonzhou@gmail.com>
2020-06-09 01:11:33 +0800

    完善卡片和包裹层组件
    卡片添加了注释, 添加了很多业务注释
    包裹层添加了多个业务事件

--- src/components/card-preview.vue
+++ src/components/card-preview.vue
@@ -1,32 +1,48 @@
 <template>
-  <view>
+  <view @tap="handleClickBox">
 
     <!-- 信息 -->
     <view class="flex solid-bottom padding-sm justify-between bg-white">
-      <view class="bg-img bg-mask bg-cover-box" @click="handlePreview(data.cover)" :style="{ backgroundImage: `url(${ data.cover })` }"></view>
-      <view class="padding-left-sm padding-right-sm text-left">
-        <view class="text-df text-black">{{ data.title }}</view>
-        <view class="text-pink">{{ data.authors && data.authors[0] }}</view>
+      <view class="flex justify-start">
+        <view class="bg-img bg-cover-box"
+          @tap.stop="handlePreview(data.cover)"
+          :style="{ backgroundImage: `url(${ data.cover })` }"
+        />
+        <view class="padding-left-sm padding-right-sm text-left">
+          <view class="text-df text-black margin-bottom" :style="{
+            maxWidth: `42vw`
+          }">{{ data.title }}</view>
+          <!-- <view class="text-pink">{{ data.authors && data.authors[0] }}</view> -->
+          <!-- <view class="padding-xs bg-white">
+            <text class="text-cut" style="width: 420rpx">{{ data.desc }}</text>
+          </view> -->
+          <!-- 标签 -->
+          <view class="flex flex-wrap bg-white" v-if="isBarInside">
+            <view class="padding-right-xs padding-bottom-xs" v-for="(item, index) in data.tags" :key="index">
+              <view class="cu-tag line-pink round">{{ item }}</view>
+            </view>
+          </view>
+        </view>
       </view>
-      <view>
+      <view class="flex align-center justify-center">
         <text style="font-size: 60rpx" class="cuIcon-favor padding-sm" />
       </view>
     </view>
 
     <!-- 标签 -->
-    <view class="padding-xs flex flex-wrap bg-white">
+    <view class="padding-xs flex flex-wrap bg-white" v-if="isBarBottom">
       <view class="padding-xs" v-for="(item, index) in data.tags" :key="index">
         <view class="cu-tag line-pink round">{{ item }}</view>
       </view>
     </view>
 
     <!-- 简介 -->
-    <view class="padding-xs bg-white">
+    <!-- <view class="padding-xs bg-white">
       <text class="text-cut" style="width: 420rpx">{{ data.desc }}</text>
-    </view>
+    </view> -->
 
     <!-- 操作栏 -->
-    <view class="flex p-xs margin-bottom-sm padding-top-sm padding-bottom-sm bg-white">
+    <view class="flex p-xs margin-bottom-sm padding-top-sm padding-bottom-sm bg-white" v-if="showActionBar">
       <view class="flex flex-treble radius flex-direction padding-left-sm padding-right-sm">
         <button class="cu-btn round bg-pink lg" @click="handleReader(data.id)">开始阅读</button>
       </view>
@@ -40,18 +56,57 @@
 </template>
 
 <script lang="ts">
+
+/**
+ * `card-preview` 预览
+ * @desc 主要是 `card`
+ * @author d1y<chenhonzhou@gmail.com>
+ * @param {Array} [data] - 数据数组
+ * @param {Boolean} [showActionBar] - 显示操作 [开始阅读 | 预览]
+ * @param {Stirng} [tagPosition] - 标签位置 **可选值[ bottom | inside ]**
+ * @event clickBox()=> void - 无返回值, 触发了中间内容区的事件
+ * @example 调用示例
+ *  <card-preview :data="" :showActionBar="false" :tagPosition="bottom" />
+ */
 import Vue from "vue";
-import { router } from '../utils';
+import { router } from '@/utils';
 export default Vue.extend({
   name: 'card-preview',
   props: {
+    /**
+     * 数据
+     */
     data: {
       required: true,
       type: Object
+    },
+    /**
+     * 显示操作 [开始阅读 | 预览]
+     */
+    showActionBar: {
+      default: true,
+      type: Boolean
+    },
+    /**
+     * 标签位置
+     * 可选值[ bottom | inside ]
+     */
+    tagPosition: {
+      default: 'bottom',
+      type: String
+    }
+  },
+  computed: {
+    isBarBottom(): boolean {
+      return this.tagPosition == 'bottom'
+    },
+    isBarInside(): boolean {
+      return this.tagPosition == 'inside'
     }
   },
   methods: {
     handlePreview(...imgs: any[]) {
+      if (!imgs) return
       uni.previewImage({
         urls: imgs,
         current: ""
@@ -61,6 +116,9 @@ export default Vue.extend({
       router.push(`reader/index`, {
         id
       })
+    },
+    handleClickBox() {
+      this.$emit('clickBox')
     }
   }
 });
@@ -68,9 +126,9 @@ export default Vue.extend({
 
 <style scoped>
 .bg-cover-box {
-  width: 840rpx;
+  width: 200rpx;
   max-width: 50vw;
-  height: 320upx;
+  height: 240upx;
   padding: 12rpx;
 }
 </style>
--- src/components/wrapper.vue
+++ src/components/wrapper.vue
@@ -1,41 +1,88 @@
 <template>
-  <scroll-view scroll-y :style="style">
+  <scroll-view
+    scroll-y
+    :style="style"
+    :show-scrollbar="isScrollbar"
+    :scroll-top="top"
+    :scroll-with-animation="true"
+    @scroll="handleScroll(false, $event)"
+    @scrolltolower="handleScroll('bottom', $event)"
+    @scrolltoupper="handleScroll('top', $event)"
+  >
     <slot name="default" />
   </scroll-view>
 </template>
 
 <script lang="ts">
-
 /**
  * 包裹层
  * @desc 该组件主要是为了解决 `ios` 的 `bar` 高度问题
  * @author d1y<chenhonzhou@gmail.com>
  * @date 2020年06月04日23:41:43
+ * @event `@scroll 触发事件`
+ * @param {Boolean} [isScrollbar] - 控制是否出现滚动条
  * @example 调用示例
- *  <wrapper></wrapper>
+ *  <wrapper @scroll=""></wrapper>
  */
-import Vue from 'vue'
-import cssType from 'csstype'
-import { isIos, isAndroid } from '@/utils/is'
+import Vue from "vue";
+import cssType from "csstype";
+import { isIos, isAndroid } from "@/utils/is";
 export default Vue.extend({
-  name: 'wrapper',
+  name: "wrapper",
+  props: {
+    /**
+     * 控制是否出现滚动条
+     */
+    isScrollbar: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      top: 0,
+      old: {
+        scrollTop: 0
+      }
+    };
+  },
   computed: {
     style(): cssType.Properties {
-      const info = uni.getSystemInfoSync()
-      let h = info.windowHeight || 0
-      const _b = info.statusBarHeight || 0
-      const CustomBar = (this as any).CustomBar
+      const info = uni.getSystemInfoSync();
+      let h = info.windowHeight || 0;
+      const _b = info.statusBarHeight || 0;
+      const CustomBar = (this as any).CustomBar;
       // fixbug: 在 `ios` 下, 需要减去 `statusBarHeight`
-      if (isIos()) h = h - _b
+      if (isIos()) h = h - _b;
       // fixbug: 在 `android`下, 需要减去自定义的 `CustomBar`
-      if (isAndroid()) h = h - CustomBar
-      //#ifdef H5  
-      h = h - CustomBar
+      if (isAndroid()) h = h - CustomBar;
+      //#ifdef H5
+      h = h - CustomBar;
       //#endif
       return {
-        height: `${ h }px`
+        height: `${h}px`
+      };
+    }
+  },
+  methods: {
+    handleScroll(position: string | boolean, event: any) {
+      if (!position) {
+        this.top = this.old.scrollTop
+        return
       }
+      this.$emit("scroll", {
+        position,
+        event
+      });
+    },
+    /**
+     * 跳转顶部
+     * 参考: https://ask.dcloud.net.cn/article/36612
+     */
+    goTop() {
+      const R = Math.random()
+      this.top = R
     }
   }
-})
+});
 </script>
