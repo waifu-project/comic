91cd0331d30ae4b3ca6a9f8c54784e214afbcde9
d1y <chenhonzhou@gmail.com>
2020-06-09 01:13:01 +0800

    添加搜索接口

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,9 +1,11 @@
 import { get, _post } from '@/utils/axios'
 import cherrio from 'cheerio'
 import { str2Data, str2Modal, detail2Data, comicPic2Data, comicTheme2Data } from '@/utils/share'
-import { shareIndexComicData, shareIndexData } from '@/interface'
+import { shareIndexComicData, shareIndexData, themeListInterface, shareComicFace } from '@/interface'
 import { isDev } from '@/config'
 import { createRandomColor } from '@/utils'
+import { searchOptions, searchResponseInterface } from '@/interface/tool'
+import querystring from '@/utils/qs'
 
 // 点赞某作品
 export const loveVoteAlbum = (album_id: string | number)=> {
@@ -37,6 +39,7 @@ export const getComicPic = async (id: number | string): Promise<any>=> {
   }
 }
 
+// 获取所有主题
 export const getTheme = async ()=> {
   try {
     const data = await get('/theme')
@@ -52,16 +55,17 @@ export const getPopularThemes = async ()=> {
     const data = await get('/')
     const $ = cherrio.load(data)
     const arr = $('.col-lg-2.col-md-3.cloud.hidden-xs.hidden-sm a')
-    const lists = Array.from(arr).map(item=> {
+    const lists: themeListInterface[] = Array.from(arr).map(item=> {
       const ele = cherrio(item)
-      const api = ele.attr('href')
+      const api = ele.attr('href') || ""
       const text = ele.text().trim()
-      const bg = `bg-${ createRandomColor().name }`
-      return {
-        api,
+      const bg = createRandomColor()
+      const obj: themeListInterface = {
+        url: api,
         text,
         bg
       }
+      return obj
     })
     return lists
   } catch (error) {
@@ -73,7 +77,7 @@ export const getPopularThemes = async ()=> {
 export const getIndexData = async (): Promise<shareIndexData>=>{
   try {
     let devUrl = `http://192.168.1.190:8081`
-    let nowUrl = (isDev && true) ? devUrl : `/`
+    let nowUrl = isDev ? devUrl : `/`
     const data = await get(nowUrl)
     const $ = cherrio.load(data)
     const modal = str2Modal($)
@@ -98,3 +102,45 @@ export const getIndexData = async (): Promise<shareIndexData>=>{
     throw new Error(error)
   }
 }
+
+// 节点转为`text`
+const node2text = (x: any): string => cherrio(x).text().trim()
+
+// 搜索
+export const getSearch = async (qs: string, options: searchOptions): Promise<searchResponseInterface>=> new Promise(async res=>{
+  let result: shareComicFace[] = []
+  let search_key = ''
+  let current_page = options.page
+  let total_page = 0
+  let total = ''
+  let isNext = false
+  try {
+    const api = querystring.mergeQueryString(qs, options)
+    const data = await get(api)
+    const $ = cherrio.load(data)
+    const qsEle = cherrio($('.col-xs-12.col-md-9.col-sm-8 .well.well-sm')[0])
+    const eles = qsEle.find('span.text-white')
+    search_key = node2text(eles[0])
+    // total_page = +node2text(eles[2])
+    const selectEles = $('.col-xs-12.col-md-9.col-sm-8 .visible-xs .pagination.pagination-lg li select').parent()
+    const temp_curr = +selectEles.find('select option[selected]').text().trim() 
+    const temp_last = +selectEles.find('select').children().last().text().trim()
+    if (temp_curr) current_page = temp_curr
+    if (temp_last) total_page = temp_last
+    if (current_page == total_page) isNext = false
+    if (current_page < total_page) isNext = true
+    total = node2text(eles[3])
+    const lists = $('.col-xs-6.col-sm-6.col-md-4.col-lg-3.list-col')
+    result = Array.from(lists).map(item=> str2Data(item))
+  } catch (error) {
+    console.error(error)
+  }
+  res({
+    isNext,
+    search_key,
+    current_page,
+    total_page,
+    total,
+    lists: result
+  })
+})
