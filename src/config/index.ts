85d16fd6afd86a09cc25c40745aca973bf9af340
d1y <chenhonzhou@gmail.com>
2020-06-11 01:31:53 +0800

    修改为注入的变量

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,18 +1,14 @@
 import { getMirror } from '@/utils/mirror'
 
+const injection = require('@/plugins/injection')
+
 export const baseUrl: string = getMirror()
 
 export default {
   baseUrl
 }
 
-export let version: string = ''
+export let version: string = injection.packages.version
 export let readme: string = ''
-try {
-  const _P: any = require('../../package.json')
-  version = _P.version
-} catch (error) {
-  console.error('读取失败, 错误栈: ', error)
-}
 
 export const isDev = process.env.NODE_ENV === 'development'
