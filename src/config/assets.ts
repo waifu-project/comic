e482de6ae625d8222addcd2e1eca23eee39029cf
d1y <chenhonzhou@gmail.com>
2020-07-04 14:54:08 +0800

    :heart: loading 静态资源
    这样图片有 `1m` 大小, 是有点大了

---
+++ src/config/assets.ts
@@ -0,0 +1,9 @@
+
+/**
+ * 加载图片
+ * link: https://giphy.com/gifs/uzfGv0BtxrWGk
+ * 以下为参考图片
+ * https://tenor.com/view/anime-loading-sending-virtual-hug-hug-gif-12129229
+ * https://tenor.com/view/anime-dance-cute-muse-dash-girl-gif-17666530
+ */
+export let _loadingImage: string = require("../static/miku_loading.gif")
